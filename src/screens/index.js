113dbe4817bac9717f33f7708c5c34c75dbf6c98
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-05-08 19:13:59 -0400

    Import and export all screens together from one file

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Home from './Home'
+import Cam from './Camera'
+import Favorites from './Favorites'
+import Login from './Login'
+
+export { Home, Cam, Favorites, Login };
